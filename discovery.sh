78c47092417f513c34c7589be429ad2c30a74deb
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 20:25:47 +0100

    Update discovery.sh

--- discovery.sh
+++ discovery.sh
@@ -28,18 +28,18 @@ function voltage()
 #
 $mqttpub -t "homeassistant/sensor/${id}/${1}/config" \
 -m '{
- "unit_of_measurement":"V",
- "device_class":"voltage",
- "state_class":"measurement",
- "name":"'"${name} ${2}"'",
- "state_topic":"'"han2mqtt/${id}/${1}"'",
- "value_template":"{{ fffggg }}",
- "unique_id":"'"${id}_$1"'",
- "device":{
-   "identifiers":"'${id}'",
-   "model":"'${id}'",
-    }
-  }'
+  "unit_of_measurement": "V",
+  "device_class": "voltage",
+  "state_class": "measurement",
+  "name": "'"${name} ${2}"'",
+  "state_topic": "'"han2mqtt/${id}/${1}"'",
+  "value_template": "{{ fffggg1 }}",
+  "unique_id": "'"${id}_$1"'",
+  "device": {
+    "identifiers": "'${id}'",
+    "model": "'${id}'",
+  }
+}'
 #
 }
 
