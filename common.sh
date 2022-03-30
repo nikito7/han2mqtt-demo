da7f8174e22c0a50dc783d459a61ad6a2763de6e
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 21:03:13 +0100

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
 
+name="EB1"
+id="han2mqtt_eb1"
+model=han2mqtt
+secrets=/config/secrets.yaml
+
+###
+
+function get()
+{
+cat $secrets | grep $1 | awk -F ": " '{ print $2 }'
+}
+
+###
+
+mqttpub="mosquitto_pub -h \
+$(get mqtt_server) -u \
+$(get mqtt_user) -P \
+$(get mqtt_pass)"
+
+###
+
+
+
+#EOF
