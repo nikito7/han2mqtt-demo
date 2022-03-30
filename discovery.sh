1b81e915ad0d39c5f73d1d5c88b8a5a0308c8d7f
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 21:04:03 +0100

    Update discovery.sh

--- discovery.sh
+++ discovery.sh
@@ -2,26 +2,7 @@
 
 ### discovery.sh ###
 
-function get()
-{
-cat $secrets | grep $1 | awk -F ": " '{ print $2 }'
-}
-
-### 
-
-name="EB1"
-id="han2mqtt_eb1"
-model=han2mqtt
-secrets=/config/secrets.yaml
-
-###
-
-mqttpub="mosquitto_pub -h \
-$(get mqtt_server) -u \
-$(get mqtt_user) -P \
-$(get mqtt_pass)"
-
-###
+. common.sh
 
 function voltage()
 {
