fd9951e461ba9c696ef3db072da1a601d38bbceb
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 20:44:38 +0100

    Update poller.sh

--- poller.sh
+++ poller.sh
@@ -10,6 +10,17 @@ HOST="-m enc -p 9502 10.1.0.37"
 #stty -F $TTY cs8 9600
 #HOST="-b 9600 -p none -m rtu $TTY"
 
+###
+
+function get16bits()
+{
+/config/apps/modpoll -a 1 -0 -1 \
+-r $1 -c $2 -t 3 $HOST \
+| grep '^\[' | awk -F ": " '{ print $2 "," }'
+}
+
+###
+
 function get32bits()
 {
 /config/apps/modpoll -a 1 -0 -1 \
@@ -17,7 +28,9 @@ function get32bits()
 | grep '^\[' | awk -F ": " '{ print $2 "," }'
 }
 
-result=$(get32bits 38 3)
+###
+
+result=$(get16bits 108 3)
 
 echo $result | sed -e 's/ //g'
 
