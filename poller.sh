2308fd3207ae8a902da1f9b81c028c1f1df5dc17
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 20:36:18 +0100

    Update poller.sh

--- poller.sh
+++ poller.sh
@@ -2,13 +2,13 @@
 
 # rtu over tcp
 
-#HOST="-m enc -p 9502 10.1.0.37"
+HOST="-m enc -p 9502 10.1.0.37"
 
 # serial
 
-TTY="/dev/ttyS3"
-stty -F $TTY cs8 9600
-HOST="-b 9600 -p none -m rtu $TTY"
+#TTY="/dev/ttyS3"
+#stty -F $TTY cs8 9600
+#HOST="-b 9600 -p none -m rtu $TTY"
 
 function get32bits()
 {
